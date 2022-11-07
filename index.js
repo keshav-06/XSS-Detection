b7c2044129f5df007dfd17e9bc0f1c29e0830bde
Keshav Varshney <87660591+keshav-06@users.noreply.github.com>
2022-11-07 09:20:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let u = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+    document.getElementById("url").innerHTML = u
+        //     fetch('http://localhost:3000/')
+        //   .then(response => response.json())
+        //   .then(data => console.log(data));
+
+    var url = new URL('http://localhost:3000/')
+
+    var params = { link: u } // or:
+
+    url.search = new URLSearchParams(params).toString();
+
+    fetch(url).then(function(response) {
+        response.text().then(function(data) {
+            if (data.includes('False')) {
+                document.getElementById("head").textContent = "This website is not XSS Vulnerable"
+            } else {
+                document.getElementById("head").innerHTML = "<h3>WARNING !! The website is XSS vulnerable</h3>"
+                document.getElementById("url").innerHTML = data
+            }
+        });
+    }).catch(function(error) {
+        console.log('Fetch Error:', error);
+    });
+});
